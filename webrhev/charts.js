0152751e826f20513782b76439e9f0db36ec3628
wgdaniel <wallace@requiem.gateway.2wire.net>
2013-10-25 23:41:51 -0400

    Web UI "Improvements"
    Trying to work out the Google Chart + jQuery combo is not easy. As it
    stands, there is code in place to switch between multiple tabs in 2
    different DIVs. Google charts does not seem to render when any jQuery
    code is called

--- webrhev/charts.js
+++ webrhev/charts.js
@@ -19,25 +19,9 @@ function drawChart() {
           ['USA',       'America',            52,                              31],
           ['Mexico',    'America',            24,                              12],
           ['Canada',    'America',            16,                              -23],
-          ['France',    'Europe',             42,                              -11],
-          ['Germany',   'Europe',             31,                              -2],
-          ['Sweden',    'Europe',             22,                              -13],
-          ['Italy',     'Europe',             17,                              4],
-          ['UK',        'Europe',             21,                              -5],
-          ['China',     'Asia',               36,                              4],
-          ['Japan',     'Asia',               20,                              -12],
-          ['India',     'Asia',               40,                              63],
-          ['Laos',      'Asia',               4,                               34],
-          ['Mongolia',  'Asia',               1,                               -5],
-          ['Israel',    'Asia',               12,                              24],
-          ['Iran',      'Asia',               18,                              13],
-          ['Pakistan',  'Asia',               11,                              -52],
-          ['Egypt',     'Africa',             21,                              0],
-          ['S. Africa', 'Africa',             30,                              43],
-          ['Sudan',     'Africa',             12,                              2],
-          ['Congo',     'Africa',             10,                              12],
-          ['Zaire',     'Africa',             8,                               10]
+          ['France',    'Europe',             42,                              -11]
     ]);
+    console.log(tree_data);
 	
 	// Create data for org chart
 	var org_data = new google.visualization.DataTable();
@@ -54,7 +38,9 @@ function drawChart() {
     ]);
 
     // Create tree chart
+    console.log("Attempting to draw the fucking tree..");
     var tree_chart = new google.visualization.TreeMap(document.getElementById('tree_div'));
+    console.log(tree_chart);
     tree_chart.draw(tree_data, {
 		  minColor: '#f00',
           midColor: '#ddd',
--- webrhev/effects.js
+++ webrhev/effects.js
@@ -1,15 +1,22 @@
-$(document).ready(function() {
+function initTabs() {
 
+	console.log("WOOOO HOOO I'VE BEEN CALLED");
 	//initialize with all divs under #tabs hidden
 	$('.left div').hide();
 	$('.right div').hide();
+	console.log("Hid the divs on either side");
 
 	//enable the first content div and tab so they are showing
 	$('.left div:first').show();
 	$('.left ul li:first').addClass('active');
+	console.log("Revealed the first left div, added active class to tab");
 
-	$('.right div:first').show();
+	$('.right div:first').show(function() {
+			drawChart();
+	});
+	
 	$('.right ul li:first').addClass('active');
+	console.log("Revealed the first right dev, added active class to tab");
 
 	//loop for reacting to tab clicks
 	$('.left ul li a').click(function(){
@@ -20,6 +27,7 @@ $(document).ready(function() {
 		$(currentTab).show();
 		return false;
 	});
+	
 
 	$('.right ul li a').click(function(){
 		$('.right ul li').removeClass('active');
@@ -32,4 +40,4 @@ $(document).ready(function() {
 
 
 
-});
+}
